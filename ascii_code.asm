e1c1d1262068c1ae025da8fd870594ed6ddee9c3
Vlad Ganusceac <36600228+VladGanuscheak@users.noreply.github.com>
2018-10-19 23:17:59 +0300

    Add files via upload

---
+++ ascii_code.asm
@@ -0,0 +1,35 @@
+
+
+ORG 7c00h
+
+add dh, 2
+mov dl, 10
+mov bh, 0
+mov ah, 2
+int 10h 
+
+mov cx, 0
+mov di, 0
+
+mov al, 0      
+mov ah, 0eh
+start_loop:
+int 10h
+
+push ax
+mov al, 9
+int 10h
+pop ax
+ 
+inc al
+
+cmp al, 0
+jne start_loop
+
+end_loop:
+
+ret
+
+
+
+
