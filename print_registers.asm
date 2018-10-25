7505d8e2341b58dffab30c772291e1887115e605
Vlad Ganusceac <36600228+VladGanuscheak@users.noreply.github.com>
2018-10-25 14:36:31 +0300

    Update print_registers.asm

--- print_registers.asm
+++ print_registers.asm
@@ -3,13 +3,13 @@
 
 ORG 7c00h    
     
-    axRegister equ 0BAFh
-    cxRegister equ 0CABh
-    dxRegister equ 0FAECh
-	bxRegister equ 0ABCEh
-	bpRegister equ 0CEFAh
-	siRegister equ 0FAC3h
-	diRegister equ 0AFBCh
+ axRegister equ 09EFh
+ cxRegister equ 0CACh
+ dxRegister equ 0DDECh
+ bxRegister equ 0AACEh
+ bpRegister equ 00EFAh
+ siRegister equ 0F0C3h
+ diRegister equ 000BCh
 
     newLine equ 0Dh, 0Ah
 
